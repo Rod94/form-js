ac7b32b9f03763aad230bab940e5f170d59bb7f4
Rodolpho Caetano <rphfti@hotmail.com>
2021-04-12 17:08:49 -0300

    box-shadow

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const sname = document.getElementById("sname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
